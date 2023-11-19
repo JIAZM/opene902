c42534060dce358a510cb5eb2d1412fdb1071c11
Sisyphe <jiazm556@163.com>
2023-11-19 22:15:46 +0800

    基于release版本做本地适配

--- smart_run/setup/smart_cfg.mk
+++ smart_run/setup/smart_cfg.mk
@@ -73,10 +73,10 @@ coremark_build:
 
 
 hello_world_build:
-	@cp ./tests/cases/hello_world/* ./work
+	@cp ./tests/cases/hello_world/* ./work -rf
 	@find ./tests/lib/ -maxdepth 1 -type f -exec cp {} ./work/ \; 
-	@cp ./tests/lib/clib/* ./work
-	@cp ./tests/lib/newlib_wrap/* ./work
+	@cp ./tests/lib/clib/* ./work -rf
+	@cp ./tests/lib/newlib_wrap/* ./work -rf
 	@cd ./work && make -s clean && make -s all CPU_ARCH_FLAG_0=e902m  ENDIAN_MODE=little-endian CASENAME=hello_world FILE=hello_world_main >& hello_world_build.case.log 
 
 
